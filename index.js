125c7f32db98183d34563d13c3d5c5cb58bb4333
Milo Canseco <canseco3rd@gmail.com>
2025-10-19 09:00:37 -0700

    Created a cuntion.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const userOrder = prompt("Hello! What froyo flayors would you like?: ");
+function froyo() {
+  const froyoFlavor = [];
+  const userOrder = prompt(
+    "Hello! What froyo flayors would you like?",
+    "Vanilla, Vanilla, vanilla, Strawberry, Coffee, coffee"
+  );
+  return;
+}
+
+console.log(froyo);
