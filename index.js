a653f27819b431c2b2217b3ace953863b75230dd
Milo Canseco <canseco3rd@gmail.com>
2025-10-19 17:14:34 -0700

    Created a function to count the occurrence of each flavor and return an order summary.

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-function froyo() {
-  const froyoFlavor = [];
-  const userOrder = prompt(
-    "Hello! What froyo flayors would you like?",
-    "Vanilla, Vanilla, vanilla, Strawberry, Coffee, coffee"
-  );
-  return;
+const userOrder = prompt("Hello! What froyo flavors would you like?");
+("Vanilla, Vanilla, Vanilla, Strawberry, Coffee, Coffee");
+const froyoFlavor = userOrder.split(",");
+
+// Count the occurrences of each flavor
+const flavorCount = {};
+
+function countFlavors() {
+  for (let i = 0; i < froyoFlavor.length; i++) {
+    const flavor = froyoFlavor[i].toLowerCase();
+
+    if (flavorCount[flavor]) {
+      flavorCount[flavor] += 1;
+    } else {
+      flavorCount[flavor] = 1;
+    }
+  }
+
+  // Generates users order summary.
+  let message = "You have ordered:\n";
+  for (const flavor in flavorCount) {
+    message += `${flavorCount[flavor]} ${flavor}\n`;
+  }
+  return message;
 }
 
-console.log(froyo);
+console.log(countFlavors());
